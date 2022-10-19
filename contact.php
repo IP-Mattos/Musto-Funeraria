4a761d3dffbe9b8065ca23bf32d646dff4a10ff9
Ivan Ignacio Peña Mattos <76266733+IP-Mattos@users.noreply.github.com>
2022-10-19 18:09:45 -0300

    Landing page musto

---
+++ contact.php
@@ -0,0 +1,24 @@
+<?php
+$nombre = $_POST['name'];
+$last_name = $_POST['last_name'];
+$mail = $_POST['email'];
+$telefono = $_POST['phone'];
+$cliente = $_POST['message'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . "".$last_name . ",\r\n";
+$mensaje .= "Su e-mail es: " . $mail . " \r\n";
+$mensaje .= "Su telefono es: " . $telefono . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['message'] . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'darkmasterofshadow@gmail.com';
+$asunto = 'Contactos Musto';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+?>
---
+++ js/index.js
@@ -0,0 +1,33 @@
+let slider = document.querySelector(".slider-contenedor");
+let sliderIndividual = document.querySelectorAll(".contenido-slider");
+let contador = 1;
+let width = sliderIndividual[0].clientWidth;
+let intervalo = 3000;
+let enter = true;
+
+window.addEventListener("resize", function () {
+  width = sliderIndividual[0].clientWidth;
+});
+slider.addEventListener("mouseenter", () => {
+  enter = false;
+});
+slider.addEventListener("mouseleave", () => {
+  enter = true;
+});
+setInterval(function () {
+  if (enter) slides();
+}, intervalo);
+
+function slides() {
+  slider.style.transform = "translate(" + -width * contador + "px)";
+  slider.style.transition = "transform .8s";
+  contador++;
+
+  if (contador == sliderIndividual.length) {
+    setTimeout(function () {
+      slider.style.transform = "translate(0px)";
+      slider.style.transition = "transform 0s";
+      contador = 1;
+    }, 1500);
+  }
+}
